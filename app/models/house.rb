a15b7203a293c20ea0a3c861062730421a475393
kevin.bett <kevin.bett@gmail.com>
2009-06-07 16:03:11 +0800

    added rspec. added navigation and validations for house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,18 @@
 class House < ActiveRecord::Base
+
+	#validates_presence_of :title
+	validates_length_of :title, :within => 10..60
+	validates_presence_of :teaser
+	validates_presence_of :description
+	validates_presence_of :price
+	validates_presence_of :payment
+	validates_presence_of :bedrooms
+	validates_presence_of :bathrooms
+	validates_presence_of :maidsrooms
+	validates_presence_of :floor_area
+	validates_presence_of :lot_area
+	
+
 	belongs_to :location
 	belongs_to :term
 	
