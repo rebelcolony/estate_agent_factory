4c44b91c4d056849836a52de354bbcc5a25ca4ac
kevin.bett <kevin.bett@gmail.com>
2009-06-07 18:44:44 +0800

    added truncate to teaser text. styled notifications and error messages

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,16 +2,9 @@ class House < ActiveRecord::Base
 
 	#validates_presence_of :title
 	validates_length_of :title, :within => 10..60
-	validates_presence_of :teaser
-	validates_presence_of :description
-	validates_presence_of :price
-	validates_presence_of :payment
-	validates_presence_of :bedrooms
-	validates_presence_of :bathrooms
-	validates_presence_of :maidsrooms
-	validates_presence_of :floor_area
-	validates_presence_of :lot_area
-	
+	validates_length_of :teaser, :within => 100..600
+	validates_presence_of :teaser, :description, :price, :payment, :bedrooms, :bathrooms, :maidsrooms, :floor_area, :lot_area
+	validates_numericality_of :price, :bedrooms, :bathrooms, :maidsrooms, :floor_area, :lot_area
 
 	belongs_to :location
 	belongs_to :term
