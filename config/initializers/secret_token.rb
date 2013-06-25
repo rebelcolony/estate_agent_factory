3b8943109dd51d4e64eddf5e838dd05e4752c18a
rebelcolony <kevin@rebelcolony.com>
2013-06-25 13:11:31 +0800

    restart development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goldhouse::Application.config.secret_token = '2d222e9c6e5fe15d648c5c4201b760320415f59f9b71dcd2c5a9cedd4223516f1f5663146d96d3a944f1dc7191fa5918917d761fb55c06a9bc1b7ffc91f388c8'
